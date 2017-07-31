bcda9c89d6b956809c9efb0f9ab7971f182a5ceb
Seth Watson <seth.watson1@gmail.com>
2017-07-30 23:53:20 -0600

    Imported Header in indexjs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// import components
+import Header from "./header";
+
 class App extends React.Component {
     render() {
         return (
             <div>
-                Test
+                <Header />
             </div>
         )
     }

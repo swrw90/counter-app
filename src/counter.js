c65836d23fded40713662d2c82aaa56ff00d24fe
Seth Watson <seth.watson1@gmail.com>
2017-07-31 00:49:29 -0600

    Created counter component

---
+++ src/counter.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Counter extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>counter</h3>
+                <button>Up</button>
+                <button>Down</button>
+            </div>
+        )
+    }
+}
+
+export default Counter;
